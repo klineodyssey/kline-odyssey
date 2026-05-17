d0cd5dc9701280ecdaba486160f8ffd89375544f
klineodyssey <mydg0795@gmail.com>
2026-05-18 04:51:53 +0800

    Add files via upload

--- K線西遊記/temples/12345/modules/kgen-12345-version.js
+++ K線西遊記/temples/12345/modules/kgen-12345-version.js
@@ -1,18 +1,25 @@
 /*
 PRODUCT_ID: KGEN-12345-HEART-UI
 MODULE: kgen-12345-version.js
-VERSION: V10.39.0_RUNTIME_GOVERNANCE
-BUILD: 20260517-V10.39.0-RUNTIME-GOVERNANCE
-RULE: Official filename has no version number.
+VERSION: V10.39.1_TEMPLE_ARCHITECTURE_MASTER
+BUILD: 20260517-V10.39.1-TEMPLE-ARCHITECTURE-MASTER
+BASE_FROM: KGEN_12345_V10_39_0_RUNTIME_GOVERNANCE_FULL.zip
+RULE: Official filename is fixed. Version is written here, not in filename.
 */
-window.KGEN_12345_VERSION = {
-  PRODUCT_ID: "KGEN-12345-HEART-UI",
-  VERSION: "V10.39.0_RUNTIME_GOVERNANCE",
-  BUILD: "20260517-V10.39.0-RUNTIME-GOVERNANCE",
-  BASE_FROM: "KGEN_12345_V10_38_0_GENESIS_520_PANEL_RECOVERY_FULL.zip",
-  MAIN_FILE: "K線西遊記/temples/12345/index.html",
-  OFFICIAL_MODULE_ROOT: "K線西遊記/temples/12345/modules/",
-  OFFICIAL_RUNTIME_ROOT: "K線西遊記/temples/12345/modules/runtime/",
-  AXIS_RULE: "X/Y are reality plane; Z is long/short mirror axis; C is warp elevator; T is natural time; CT is current price boundary."
+
+window.KGEN_12345_VERSION={
+  PRODUCT_ID:"KGEN-12345-HEART-UI",
+  VERSION:"V10.39.1_TEMPLE_ARCHITECTURE_MASTER",
+  BUILD:"20260517-V10.39.1-TEMPLE-ARCHITECTURE-MASTER",
+  BASE_FROM:"KGEN_12345_V10_39_0_RUNTIME_GOVERNANCE_FULL.zip",
+  MAIN_FILE:"K線西遊記/temples/12345/index.html",
+  RULE:"升版改內容與 header，不改正式 module 檔名。",
+  CORE_PURPOSE:"12345 五指山悟空財神殿，以發發財金、心跳、點火、許願、還願為主，不擴充成 11520 交易所。",
+  ASSET_BINDING:{
+    "bull-front.png":"+Z 多方宇宙中央圖",
+    "bear-rear.png":"-Z 空方宇宙中央圖",
+    "heart.png":"移動暫態圖，停止後回多空圖",
+    "warp-core.png":"右下曲速電梯艙，跟隨 C=0~300 移動"
+  }
 };
-console.log("[KGEN 12345 VERSION]", window.KGEN_12345_VERSION);
+console.log("[KGEN 12345 VERSION]",window.KGEN_12345_VERSION);
